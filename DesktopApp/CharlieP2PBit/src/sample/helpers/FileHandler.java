8b4d0bc8593d44cf92665f624ef8cd7e25e4a990
tothattila97 <toth.attila9704@gmail.comT>
2019-11-05 19:45:30 +0100

    Two view, serialization ready, networking under progress

---
+++ DesktopApp/CharlieP2PBit/src/sample/helpers/FileHandler.java
@@ -0,0 +1,50 @@
+package sample.helpers;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FileHandler {
+
+    public static Map<String, String> metaDatas = new HashMap<String, String>();
+
+    public FileHandler() {
+        metaDatas.put("path", System.getProperty("user.dir") + "//metadata.ser");
+        // TODO: Remove it later
+        metaDatas.put("cucc", "valami1");
+        metaDatas.put("cucc1", "valami2");
+        metaDatas.put("cucc2", "valami3");
+    }
+
+    public void writeToFile() {
+        try{
+            FileOutputStream fileOut =  new FileOutputStream(metaDatas.get("path"));
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(metaDatas);
+            out.close();
+            fileOut.close();
+        }catch (Exception e){
+            //TODO: Log in file
+            e.printStackTrace();
+        }
+    }
+
+    public HashMap<String, String> readFromFile() {
+        HashMap<String, String> map = null;
+        try {
+            FileInputStream fileIn = new FileInputStream(System.getProperty("user.dir") + "//metadata.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            map = (HashMap<String, String>) in.readObject();
+            in.close();
+            fileIn.close();
+            return map;
+        } catch (Exception i) {
+            i.printStackTrace();
+        }
+        return map;
+    }
+
+}
---
+++ DesktopApp/CharlieP2PBit/src/sample/network/ServerConnection.java
@@ -0,0 +1,8 @@
+package sample.network;
+
+public class ServerConnection {
+
+    //TODO: HTTP request for the central server
+    public ServerConnection(){
+    }
+}
