c63eb1e1f5dd7f1d23f001b339eedecf4d8364f7
tothattila97 <toth.attila9704@gmail.comT>
2019-11-28 17:56:32 +0100

    merge from remote

--- Frontend/src/components/utils/constants.js
+++ Frontend/src/components/utils/constants.js
@@ -2,4 +2,4 @@ export const LOGIN = "/login";
 export const REGISTER = "/sign-up";
 
 export const localhostSocket = "http://127.0.0.1:4001";
-export const localhostHttp = "http://10.192.136.10:8080";
+export const localhostHttp = "http://localhost:8080";
